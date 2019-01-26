00672e2be0ed6ebe7ef128d19ea960031963c9dc
Semyon Minyushov <minyushov@gmail.com>
2019-01-26 23:35:02 +0400

    Better support of generics

--- modular-adapter/src/main/java/com/minyushov/adapter/Extensions.kt
+++ modular-adapter/src/main/java/com/minyushov/adapter/Extensions.kt
@@ -2,14 +2,22 @@ package com.minyushov.adapter
 
 import java.lang.reflect.ParameterizedType
 
-internal fun <T> Any.getGenericType(): Class<T> {
+internal fun <T : ModularItem> Any.getGenericType(): Class<T> {
   var type: Class<*>? = javaClass
   while (type?.genericSuperclass !is ParameterizedType) {
     type = type?.superclass
   }
+
   val parameterizedType = type.genericSuperclass as ParameterizedType
   @Suppress("UNCHECKED_CAST")
-  return parameterizedType.actualTypeArguments[1] as Class<T>
+  parameterizedType.actualTypeArguments.forEach { parameterType ->
+    val parameterTypeClass = parameterType as? Class<T>
+    if (parameterTypeClass != null && ModularItem::class.java.isAssignableFrom(parameterTypeClass)) {
+      return parameterType
+    }
+  }
+
+  throw IllegalArgumentException("Unable to find a subclass of ModularItem in type parameters of ${javaClass.name}")
 }
 
 internal fun <T> MutableList<T>.swap(from: Int, to: Int) {
