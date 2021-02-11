8a41e0bd1ab47fd44482a30e416ea845be39e569
Semyon Minyushov <minyushov@gmail.com>
2021-02-11 11:44:22 +0400

    Clean up gradle configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
   plugins {
     kotlin("android") version "1.4.30"
     id("com.android.application") version "7.0.0-alpha04"
+    id("com.android.library") version "7.0.0-alpha04"
   }
 }
 
